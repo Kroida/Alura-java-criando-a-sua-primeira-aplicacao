1421fc897c6b28dfd6c74ccb667193262902f825
Luís <luisGNS8022@gmail.com>
2026-05-30 17:57:49 -0300

    Tomando decisões

--- primeiroPrograma/src/Condicional.java
+++ primeiroPrograma/src/Condicional.java
@@ -1,5 +1,5 @@
 public class Condicional {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         int anoDeLancamento = 2022;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
