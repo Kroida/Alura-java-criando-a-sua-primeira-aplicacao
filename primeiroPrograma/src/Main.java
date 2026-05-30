681ea3f6c04f51736c31cd28f32e8871d019486b
Luís <luisGNS8022@gmail.com>
2026-05-30 11:13:13 -0300

    Atribuindo valores aos filmes

--- primeiroPrograma/src/Main.java
+++ primeiroPrograma/src/Main.java
@@ -1,3 +1,14 @@
 void main() {
-    System.out.println("Alô Mundo!");
+    System.out.println("Esse é o Screen Match");
+    System.out.println("Filme: Top Gun Maverick");
+
+    // Integer
+    int anoDeLancamento = 2022;
+    System.out.println("Ano de lançamento: " + anoDeLancamento);
+
+    // Boolean
+    boolean incluidoNoPlano = true;
+
+    // Double - é usado em aplicações financeiras e etc... float é pra jogos e dispositivos limitados
+    double notaDoFilme = 8.1;
 }
