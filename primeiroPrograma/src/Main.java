0d204503e174b6cca12ad1c19f06687bc9b2b03d
Luís <luisGNS8022@gmail.com>
2026-05-30 11:21:26 -0300

    Trabalhando com textos

--- primeiroPrograma/src/Main.java
+++ primeiroPrograma/src/Main.java
@@ -2,13 +2,27 @@ void main() {
     System.out.println("Esse é o Screen Match");
     System.out.println("Filme: Top Gun Maverick");
 
-    // Integer
     int anoDeLancamento = 2022;
     System.out.println("Ano de lançamento: " + anoDeLancamento);
-
-    // Boolean
     boolean incluidoNoPlano = true;
-
-    // Double - é usado em aplicações financeiras e etc... float é pra jogos e dispositivos limitados
     double notaDoFilme = 8.1;
+
+    // Ordem de prioridade na prática
+    // double media = 9.8 + 6.3 + 8.0 / 3;
+    double media = (9.8 + 6.3 + 8.0) / 3;
+    System.out.println(media);
+
+    // String pra escrever textos
+    String sinopse;
+    // sinopse = "Filme de aventura com galã dos anos 80";
+
+    // Text Blocks
+    sinopse = """
+            Filme Top Gun
+            Filme de aventura com galã dos anos 80
+            Muito bom!
+            Ano de lançamento
+            """ + anoDeLancamento;
+
+    System.out.println(sinopse);
 }
